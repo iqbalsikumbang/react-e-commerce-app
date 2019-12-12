640f68a0b01d9f1104d147628c5a5b7090a6ac92
iqbalsikumbang <56057825+iqbalsikumbang@users.noreply.github.com>
2019-12-12 14:38:47 +0700

    ag

--- src/components/Cart/PaypalButton.js
+++ src/components/Cart/PaypalButton.js
@@ -4,13 +4,9 @@ import PaypalExpressBtn from 'react-paypal-express-checkout';
 export default class MyApp extends React.Component{
     render() {
         const onSuccess = (payment) => {
-<<<<<<< HEAD
             console.log("The payment success", payment);  
             this.props.clearCart();
             this.props.history.push('/');                  
-=======
-            console.log("The payment success", payment);                     
->>>>>>> bf89d32a7d43480b70db544b0029b7f6182ae0ee
         }
         const onCancel = (data) => {
             console.log("canceled", data);            
@@ -20,21 +16,13 @@ export default class MyApp extends React.Component{
         }
         let env = "sandbox";
         let currency = "USD";
-<<<<<<< HEAD
         //let total = 1;
-=======
-        let total = 1;
->>>>>>> bf89d32a7d43480b70db544b0029b7f6182ae0ee
         const client = {
             sandbox: "process.env.REACT_APP_ID",
             production: "sb-43vb7j661938@business.example.com"
         }
         return (
-<<<<<<< HEAD
             <PaypalExpressBtn env={env} client={client} currency={currency} total={this.props.total} onError={onError} onSuccess={onSuccess} onCancel={onCancel} />
-=======
-            <PaypalExpressBtn env={env} client={client} currency={currency} total={total} onError={onError} onSuccess={onSuccess} onCancel={onCancel} />
->>>>>>> bf89d32a7d43480b70db544b0029b7f6182ae0ee
         )
     }
 }
